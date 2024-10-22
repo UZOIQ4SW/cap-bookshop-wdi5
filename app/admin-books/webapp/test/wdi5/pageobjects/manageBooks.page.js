b7d68cdd8ab89fae7e82391147ba6283e78493f5
Gaurav Phadnis <gphadnis2000@yahoo.com>
2024-10-22 10:56:31 +0000

    test

--- app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -2,7 +2,27 @@ module.exports = {
   /**
     * define actions for the page object
   */
+  iClickOnCreateNewBook:async () => {
+  await browser.asControl({
+    selector: {
+          id: "books::BooksDetailsList--fe::EditableHeaderForm::EditableHeaderTitle::Field-edit",
+          interaction: {
+                  idSuffix: "inner"
+          }
+  }}).enterText();
+},
 
+theBookListContains: async (sBook) => {
+  const text = await browser.asControl({
+    selector: {
+          controlType: "sap.m.Text",
+          viewId: "books::BooksList",
+          properties: {
+                  text: sBook
+          }
+  }}).getText();
+  expect(text).toEqual(sBook);
+}
   /**
    * define assertions for the page object
    */
--- app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
@@ -2,7 +2,73 @@ module.exports = {
         /**
          * define actions for the page object
          */
+        iEnterTextInTheControl: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::EditableHeaderForm::EditableHeaderTitle::Field-edit",
+                        interaction: {
+                                idSuffix: "inner"
+                        }
+                }}).enterText("test");
+            },
 
+            iSelectGenre: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::genre_ID::Field-edit-inner",
+                        interaction: {
+                                idSuffix: "inner"
+                        }
+                }}).press();
+                
+                        await browser.asControl({
+                                selector: {
+                                controlType: "sap.m.Text",
+                                viewId: "books::BooksDetailsList",
+                                bindingPath: {
+                                        path: "/Genres(10)",
+                                        propertyPath: "name"
+                                },
+                                searchOpenDialogs: true
+                        }}).press();
+                    
+            },
+
+            iSelectAuthor: async () => {
+                await browser.asControl({
+                        selector: {
+                        controlType: "sap.m.Text",
+                        viewId: "books::BooksDetailsList",
+                        bindingPath: {
+                                path: "/Authors(101)",
+                                propertyPath: "name"
+                        },
+                        searchOpenDialogs: true
+                }}).press();
+            },
+            iSeeEditButton: async () => {
+                const enabled = await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::StandardAction::Edit"
+                }}).getEnabled();
+                expect(enabled).toBeTruthy();
+            },
+
+            iPressCreate: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::FooterBar::StandardAction::Save",
+                        interaction: {
+                                idSuffix: "BDI-content"
+                        }
+                }}).press();
+            },
+            iNavigateBack: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "backBtn"
+                }}).press();
+            }
         /**
          * define assertions for the page object
          */
